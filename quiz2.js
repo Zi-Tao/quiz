958e11052ff315c94bbc7c08c492b629547590e5
Zi-Tao <Immatricolarsi@gmail.com>
2021-03-31 11:23:54 +0800

    quiz2

--- quiz2.js
+++ quiz2.js
@@ -1,4 +1,7 @@
 document.addEventListener("keydown",function(event){
-    alert(event.key);
+    // alert(event.key);
     // document.write(event.key);
+    $(document).ready(function(){
+        $("#div").text(event.key);
+    });
 })    
