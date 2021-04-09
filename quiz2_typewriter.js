9df43920ffabc3e1e46608ce29546a438692b35b
Zi-Tao <80264433+Zi-Tao@users.noreply.github.com>
2021-04-09 15:43:26 +0800

    typewriter

---
+++ quiz2_typewriter.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown",function(event){
+     // alert(event.key);
+     // document.write(event.key);
+     $(document).ready(function(){
+         $("#div").text(event.key);
+     });
+ })
